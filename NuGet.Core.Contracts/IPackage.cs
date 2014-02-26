5612813788b39b6e206195c467494133987609f4
Kelly Stuard <kellystuard@gmail.com>
2014-02-25 19:34:32 -0600

    Adding additional contracts for IPackageManager.

--- NuGet.Core.Contracts/IPackage.cs
+++ NuGet.Core.Contracts/IPackage.cs
@@ -1,8 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.Contracts;
+using System.Runtime.Versioning;
 
 namespace NuGet
 {
+	[ContractClass(typeof(PackageContractClass))]
 	public interface IPackage : IPackageMetadata
 	{
+		IEnumerable<FrameworkName> GetSupportedFrameworks();
+	}
+
+	[ContractClassFor(typeof(IPackage))]
+	[ExcludeFromCodeCoverage]
+	internal abstract class PackageContractClass : IPackage
+	{
+		IEnumerable<FrameworkName> IPackage.GetSupportedFrameworks()
+		{
+			Contract.Ensures(Contract.Result<IEnumerable<FrameworkName>>() != null);
+			Contract.Ensures(Contract.ForAll(Contract.Result<IEnumerable<FrameworkName>>(), c => c != null));
+			throw new NotImplementedException();
+		}
+
+		public abstract string Id { get; }
+		public abstract SemanticVersion Version { get; }
 	}
 }
